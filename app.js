763b541b023828c7a8d714d7e9db3734ea78ef1a
amandadiasmuller <amandadiasds@outlook.com>
2024-11-19 06:28:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("Bom dia Amandinha");
+let humor = prompt("Como está seu dia hoje?");
+if (humor == "bom") {
+    alert("fico muito feliz, Deus abençoe muito seu dia")
+};
+if (humor == "ruim") {
+    alert("Espero que fique tudo bem...")
+    let tecnicaResp = prompt("Que tal realizar técnicas de relaxamento como respiração?")};
+    if (tecnicaResp == "não") {
+        alert("Tudo bem e fique bem, qualquer coisa ligue ou converse com uma pessoa querida ou faça atividades que são prazerosas e que te fazem bem <3")};
+    if (tecnicaResp == "sim") {
+        alert("Inspire o ar profundamente pelo nariz e solte e pela boca... relaxe... repita o procedimento 5 vezes")
+        let tecnicaRespOk = prompt("A técnica ajudou?")
+        if (tecnicaRespOk = "sim") {
+            alert("ótimo! que seu dia continue incrível!")
+        }
+    };
